050ce99faee151251b8d80f02bfeabe48f5161f8
cynthiadata914 <chiomangene914@gmail.com>
2026-07-09 08:16:43 +0100

    amend! Clean up deleted and updated files
    Clean up deleted and updated files

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,10 +1,11 @@
 def format_name(f_name, l_name):
+    """Take my first and last name and format it to return the title case of my name"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
 
 
-formatted_name = format_name("AnGeLa", "YU")
+formatted_name = format_name("cYnthiA", "OMa")
 
 length = len(formatted_name)
 
